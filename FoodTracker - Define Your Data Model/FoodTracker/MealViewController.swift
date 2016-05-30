d547b0b73c13dd5bd01f46728c57c0e0f3d72651
giria <joan@giria.com>
2016-05-30 14:38:09 +0200

    cancel button

--- FoodTracker - Define Your Data Model/FoodTracker/MealViewController.swift
+++ FoodTracker - Define Your Data Model/FoodTracker/MealViewController.swift
@@ -72,6 +72,11 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
         dismissViewControllerAnimated(true, completion: nil)
     }
     // MARK: Navigation
+    
+    @IBAction func cancel(sender: UIBarButtonItem) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     // This method lets you configure a view controller before it's presented.
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if saveButton === sender {
