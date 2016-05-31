03bfd78ea7fd656383489b901840ab0259d26744
giria <joan@giria.com>
2016-05-31 18:34:55 +0200

    implement the method to load the meal list

--- FoodTracker - Define Your Data Model/FoodTracker/Meal.swift
+++ FoodTracker - Define Your Data Model/FoodTracker/Meal.swift
@@ -16,6 +16,12 @@ class Meal : NSObject, NSCoding {
     var photo: UIImage?
     var rating: Int
     
+    
+    // MARK: Archiving Paths
+    
+    static let DocumentsDirectory = NSFileManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.URLByAppendingPathComponent("meals")
+    
     // MARK: types
     struct PropertyKey {
         static let nameKey = "name"
@@ -31,6 +37,8 @@ class Meal : NSObject, NSCoding {
         self.photo = photo
         self.rating = rating
         
+        super.init()
+        
         // Initialization should fail if there is no name or if the rating is negative.
         if name.isEmpty || rating < 0 {
             return nil
@@ -41,6 +49,18 @@ class Meal : NSObject, NSCoding {
         aCoder.encodeObject(name, forKey: PropertyKey.nameKey)
         aCoder.encodeObject(photo, forKey: PropertyKey.photoKey)
         aCoder.encodeInteger(rating, forKey: PropertyKey.ratingKey)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        let name = aDecoder.decodeObjectForKey(PropertyKey.nameKey) as! String
+        
+        // Because photo is an optional property of Meal, use conditional cast.
+        let photo = aDecoder.decodeObjectForKey(PropertyKey.photoKey) as? UIImage
+        
+        let rating = aDecoder.decodeIntegerForKey(PropertyKey.ratingKey)
+        
+        // Must call designated initializer.
+        self.init(name: name, photo: photo, rating: rating)
     }
     
 }
